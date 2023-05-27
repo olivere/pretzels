bb83f42cd5c36970fb226b796831fd4866c6c1ed
Oliver Eilhard <oliver@eilhard.net>
2023-05-27 16:22:13 +0200

    Fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hooray. You've been hacked! Just kidding.
+      Hooray. You&apos;ve been hacked! Just kidding.
     </main>
   );
 }
