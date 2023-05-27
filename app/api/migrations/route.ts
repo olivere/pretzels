7f2a2bbc5f55cfd34597d6847f96d8dd28bef18b
Oliver Eilhard <oliver@eilhard.net>
2023-05-27 16:56:50 +0200

    Connect with PostgreSQL storage

---
+++ app/api/migrations/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const client = await db.connect();
+
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`;
+    await client.sql`CREATE TABLE IF NOT EXISTS products (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
+    );`;
+    await client.sql`INSERT INTO products (name) VALUES ('Apple iPhone 14');`;
+    await client.sql`INSERT INTO products (name) VALUES ('Apple iPhone 14 Pro');`;
+    await client.sql`INSERT INTO products (name) VALUES ('Apple MacBook Pro 16"');`;
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+
+  const products = await client.sql`SELECT * FROM products;`;
+  return NextResponse.json({ products });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { sql } from "@vercel/postgres";
+
+export default async function Home() {
+  const { rows: products } = await sql`SELECT * FROM products`;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hooray. You&apos;ve been hacked! Just kidding.
+      <div className="flex flex-col items-center justify-center">
+        {products.length === 0 ? (
+          <h1 className="text-4xl font-bold">No products found</h1>
+        ) : (
+          <>
+            <h1 className="text-4xl font-bold">Products</h1>
+            <ul className="mt-8">
+              {products.map((product) => (
+                <li key={product.id}>{product.name}</li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </main>
   );
 }
