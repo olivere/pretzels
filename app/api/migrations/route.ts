e2dfc8dddbc761499aa1804989d7f3da2e7830c2
Oliver Eilhard <oliver@eilhard.net>
2023-05-27 17:23:57 +0200

    Use POST for migrations

--- app/api/migrations/route.ts
+++ app/api/migrations/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
   const client = await db.connect();
 
   try {
